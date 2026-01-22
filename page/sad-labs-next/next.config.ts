6a0501a4c3227845345020ff144827bf3e817c52
nowemoore <nowe.moore@gmail.com>
2026-01-22 21:44:51 +0000

    configed

--- page/sad-labs-next/next.config.ts
+++ page/sad-labs-next/next.config.ts
@@ -2,12 +2,12 @@ import type { NextConfig } from "next";
 
 const repo = process.env.GITHUB_REPOSITORY?.split("/")[1] || "";
 
-const nextConfig: NextConfig = {
-  output: "export",         
-  images: { unoptimized: true }, 
-  trailingSlash: true,    
-  basePath: repo ? `/${repo}` : "",
-  assetPrefix: repo ? `/${repo}/` : "",
+const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  images: { unoptimized: true },
+  basePath: "/sad",
+  assetPrefix: "/sad/",
 };
 
 export default nextConfig;
